7d75443226a51e6401e1fc1635054d869045f8d1
Trishak Jaiswal <trishak78807@gmail.com>
2026-07-16 09:52:39 +0530

    Ui small change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Routes ,Route } from 'react-router-dom'
 import Home from './pages/Home'
-
+import UserLogin from './pages//UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainSignup  from './pages/CaptainSignup'
+import Captainlogin from './pages/Captainlogin'
 
 const App = () => {
   return (
     <Routes>
     <Route path="/" element={<Home />} />
+    <Route path ="/login" element={<UserLogin />} />
+    <Route path ="/signup" element={<UserSignup />} />
+    <Route path ="/captain-login" element={<Captainlogin />} />
+    <Route path ='/captain-signup' element={<CaptainSignup />} />
     </Routes>
   )
 }
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import homeImage from '../assets/images/Home.jpg'
 const Home = () => {
   return (
-    <div className="text-black text-3xl font-bold">Home-Page</div>
-  )
-}
+    <div
+      className="h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${homeImage})` }}
+    >
+    </div>
+  );
+};
 
 export default Home
